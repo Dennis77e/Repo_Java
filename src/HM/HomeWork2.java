2d48cacab76c423aebb25b642763359a9d1d1092
Denis N <Denis@Deniss-MacBook-Pro.local>
2020-05-04 18:11:08 -0400

    HomeWork2

---
+++ src/HM/HomeWork2.java
@@ -0,0 +1,48 @@
+package HM;
+
+public class HomeWork2 {
+
+    public static void main(String[] args0) {
+
+        String name = "Denis";
+        String last = "Garmaev";
+
+        String myDetails = name + " " + last;
+
+        System.out.println("My Details are: " + myDetails);
+
+        int nameLenght = name.length();
+        System.out.println("Lenght of '" + name + " is " + nameLenght );
+
+        boolean doesStartsWithK = name.startsWith("K");
+        System.out.println("Does Starts With K? : " + doesStartsWithK );
+
+        boolean doesStartswithk = name.startsWith("k");
+        System.out.println("Does Starts with k? : " + doesStartswithk );
+
+        boolean doesEndsWithM = name.endsWith("M");
+        System.out.println("Does Ends With M? : " + doesEndsWithM );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
