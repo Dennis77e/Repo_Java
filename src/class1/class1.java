cbf3ff39b92d5fabff147010fa0f6c67ba2f2a78
Denis N <Denis@Deniss-MacBook-Pro.local>
2020-05-03 20:26:54 -0400

    HomeWork

---
+++ src/class1/class1.java
@@ -0,0 +1,16 @@
+package class1;
+
+public class class1 {
+
+    // split
+    /** "hello how are you? I am good."
+     *
+     *
+     * "hello" "how" "are" "you?" "I" "am" "good."
+     *
+     *
+     *
+     */
+
+
+}
---
+++ src/labclass/classlab.java
@@ -0,0 +1,10 @@
+package labclass;
+
+public class classlab {
+
+public static void main (String [] agrs)
+
+    String threeWordString = "hello dear how are you doing?";
+    boolean isThreeWordsInString = 
+
+}
